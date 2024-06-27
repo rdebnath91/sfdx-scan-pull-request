b00001a08cf3c3477d3126c526fb2f6f586a2fe7
balakrishnaraorj <bala.rj443@gmail.com>
2024-06-27 16:14:08 +0300

    remove remote update

--- src/git-actions.ts
+++ src/git-actions.ts
@@ -43,7 +43,7 @@ export async function getDiffInPullRequest(
     baseRef,
     headRef,
     });
-    execSync(`git remote update`);
+    //execSync(`git remote update`);
 	  console.log("STEP 2 ...", {
     baseRef,
     headRef,
